1a48e5d6412f401137ca8e59503c41d2f8700c20
Bagus Windhu Aponsa <windunc@gmail.com>
2022-08-08 10:25:03 +0700

    Configuration Model

--- app/Models/AdvantageService.php
+++ app/Models/AdvantageService.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class AdvantageService extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    use SoftDeletes;
+
+    public $table = 'advantage_service';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'users_id',
+        'advantage',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 }
--- app/Models/AdvantageUser.php
+++ app/Models/AdvantageUser.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class AdvantageUser extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    use SoftDeletes;
+
+    public $table = 'advantage_user';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'service_id',
+        'advantage',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 }
